2f7ba9968a2930f0c35f8c57a5f8cb2b787fa0a4
Filip Razek <filip.razek@protonmail.com>
2024-03-12 10:45:25 +0100

    :sparkles: Display catalog items on page

--- src/java/logic/CatalogManager.java
+++ src/java/logic/CatalogManager.java
@@ -1,7 +1,9 @@
 package logic;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.io.Serializable;
+import jakarta.annotation.PostConstruct;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.ApplicationScoped;
 
@@ -14,6 +16,12 @@ public class CatalogManager implements Serializable {
     private List<Product> productList;
 
     public CatalogManager() {
+    }
+
+    @PostConstruct
+    public void initCatalog() {
+        productList = new ArrayList<>();
+        productList.add(new Product(0, "Masque de ski", 149.99));
     }
 
     public List<Product> getProductList() {
