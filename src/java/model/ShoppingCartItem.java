f365c3bac713ee4e81f9fa5547b80db01685003a
Filip Razek <filip.razek@protonmail.com>
2024-03-12 11:55:40 +0100

    :sparkles: Create ShoppingCartItem JavaBean

---
+++ src/java/model/ShoppingCartItem.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.io.Serializable;
+
+public class ShoppingCartItem implements Serializable {
+
+    private Integer id;
+    private Integer amount;
+    private Product product;
+
+    public ShoppingCartItem() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+}
